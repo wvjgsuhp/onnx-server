6a88b02f1b91dd937c52b6b97b1cef7745ad8574
wasin pipattungsakul <wasin.pas@hotmail.com>
2021-04-07 12:55:26 +0700

    log time

--- src/neuralnetwork/routes.rs
+++ src/neuralnetwork/routes.rs
@@ -2,17 +2,25 @@ use crate::neuralnetwork::*;
 use actix_web::{post, web, HttpResponse, Responder};
 use onnxruntime::{ndarray::Array, session::Session, tensor::OrtOwnedTensor};
 use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 
 #[post("/predict")]
 async fn predict(
     session: web::Data<Arc<Mutex<Session<'static>>>>,
     image_uploaded: web::Json<Image>,
 ) -> impl Responder {
-    let mut fashion_mnist_session = session.lock().unwrap();
+    // load image
+    let load_img_start = Instant::now();
 
     let img = Image::load(&image_uploaded.location).into_luma8().to_vec();
-    // println!("{:?}", img);
 
+    let load_img_duration = load_img_start.elapsed();
+    println!("image load time: {:?}", load_img_duration);
+
+    // inference
+    let inference_start = Instant::now();
+
+    let mut fashion_mnist_session = session.lock().unwrap();
     let input_shape: Vec<usize> = fashion_mnist_session.inputs[0]
         .dimensions()
         .map(|d| d.unwrap())
@@ -23,6 +31,9 @@ async fn predict(
     let output: Vec<OrtOwnedTensor<f32, _>> = fashion_mnist_session
         .run(input_tensors)
         .expect("predict failed");
+
+    let inference_duration = inference_start.elapsed();
+    println!("inference time: {:?}", inference_duration);
 
     HttpResponse::Ok().body(format!("{:?}", output))
 }
